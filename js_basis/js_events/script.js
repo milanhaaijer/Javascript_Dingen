fb31fed8c6931fcca532e9992c4b8decbdfef5fc
milanhaaijer <97105324@st.deltion.nl>
2024-01-11 11:33:36 +0100

    js opdrachten
    js arrays & js events

---
+++ js_basis/js_events/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    console.log(".");
+}
+
+function keyUp() {
+    let ageInput = document.getElementById("age").value;
+    document.getElementById("output").innerHTML = ageInput;
+}
